7d41eb135718922fd912148bc691aafe5a3bbbf9
Jeroen Bleijenberg <jeroen@maxserv.com>
2015-06-22 16:31:21 +0200

    Added function to process known hosts

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -8,4 +8,14 @@ node default {
   if defined(Class['mariadb']) {
     create_resources(mariadb::database, hiera('databases'))
   }
+
+  if defined(Class['solr']) {
+    create_resources(solr::core, hiera('solr-cores'))
+  }
+
+  if (hiera('known_hosts')) {
+    $hosts = hiera('known_hosts')
+
+    known_hosts::host{ $hosts: }
+  }
 }
